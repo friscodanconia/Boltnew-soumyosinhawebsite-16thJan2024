d2e7eb4d533bd590a3a007511ce9be1be5052374
Soumyo jyoti Sinha <Soumyo@Soumyos-MacBook-Pro.local>
2025-03-14 20:03:32 -0700

    Fix Sanity configuration and TypeScript types

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,19 @@
-export const config = {
-  projectId: 'i4pcg2lx',
-  dataset: 'production',
-  apiVersion: '2024-01-26',
-  useCdn: true,
-  token: import.meta.env.VITE_SANITY_TOKEN || 'skqkhmxBIOywCpNylr4YCtIf4nQLtOXYQw5UH4L4LlzwRynIAE5fPCMwuykEyHZlOEYXpxGOaqdHqbJqjHQf7ii7faSHb2QCGWFJlHVguCkAZZnmwvbWrVIb7H8JrUs1p1hTttDGJYQNDZoMzn6vlpDdx5J8SjN9I66JGSYVRUu683n0Oh5c'
+import { createClient } from '@sanity/client';
+
+declare global {
+  interface ImportMeta {
+    env: {
+      VITE_SANITY_PROJECT_ID: string;
+      VITE_SANITY_DATASET: string;
+      VITE_SANITY_TOKEN: string;
+    }
+  }
 }
+
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  token: import.meta.env.VITE_SANITY_TOKEN,
+  useCdn: true,
+  apiVersion: '2024-01-26',
+});
