30a4d703fd57710d2f1e08e7e901b9b243b5f643
Soumyo jyoti Sinha <Soumyo@Soumyos-MacBook-Pro.local>
2025-03-14 19:51:19 -0700

    Update Sanity config: Enable CDN and add TypeScript env types

---
+++ sanity.config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  projectId: 'i4pcg2lx',
+  dataset: 'production',
+  apiVersion: '2024-01-26',
+  useCdn: true,
+  token: import.meta.env.VITE_SANITY_TOKEN || 'skqkhmxBIOywCpNylr4YCtIf4nQLtOXYQw5UH4L4LlzwRynIAE5fPCMwuykEyHZlOEYXpxGOaqdHqbJqjHQf7ii7faSHb2QCGWFJlHVguCkAZZnmwvbWrVIb7H8JrUs1p1hTttDGJYQNDZoMzn6vlpDdx5J8SjN9I66JGSYVRUu683n0Oh5c'
+}
